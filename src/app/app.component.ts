468e96b9919939caa4357bee73ab81fc117fbb85
sko95webkiller <skortasa@gmail.com>
2022-10-22 13:29:10 +0200

    V1.4 usage de la directive *ngFor et tableau d'objet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  my2Snap!: FaceSnap;
-  my3Snap!: FaceSnap;
+  mySnap!: FaceSnap[];
+  //my2Snap!: FaceSnap;
+  //my3Snap!: FaceSnap;
 
   ngOnInit() {
+    //VERSION AVEC CONSTRUCTEUR
     // this.mySnap = new FaceSnap(
     //   'Mokhtarus',
     //   'Mon ami de toujours',
@@ -20,30 +21,43 @@ export class AppComponent implements OnInit {
     //   'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
     // );
 
+    //VERSION AVEC OBJET ISOLE
+    // this.mySnap = {
+    //   title: 'Mokhtarus',
+    //   description: 'Mon ami de toujours',
+    //   createdDate: new Date(),
+    //   snaps: 600,
+    //   imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+    //   location: 'Paris'
+    // }
+   
+    //VERSION AVEC UN TABLEAU D OBJET
+    this.mySnap = [
+      {
+        title: 'Mokhtarus',
+        description: 'Mon ami de toujours',
+        createdDate: new Date(),
+        snaps: 600,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location: 'Paris'
+      },
+      {
+        title: 'ben mon ami',
+        description: 'chien de la casse',
+        createdDate: new Date(),
+        snaps: 1200,
+        imageUrl: 'https://picsum.photos/200/300',
+      },
+      {
+        title: 'Aktarus',
+        description: 'Mon ami de toujours',
+        createdDate: new Date(),
+        snaps: 999,
+        imageUrl: 'https://picsum.photos/id/200/300',
+        location: 'New York'
+      }
+    ]
 
-    this.mySnap = {
-      title: 'Mokhtarus',
-      description: 'Mon ami de toujours',
-      createdDate: new Date(),
-      snaps: 600,
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      location: 'Paris'
-    }
-    this.my2Snap = {
-      title: 'ben mon ami',
-      description: 'chien de la casse',
-      createdDate: new Date(),
-      snaps: 1200,
-      imageUrl: 'https://picsum.photos/200/300',
-    }
-    this.my3Snap = {
-      title: 'Aktarus',
-      description: 'Mon ami de toujours',
-      createdDate: new Date(),
-      snaps: 999,
-      imageUrl: 'https://picsum.photos/id/200/300',
-      location: 'New York'
-    }
 
   }
 
